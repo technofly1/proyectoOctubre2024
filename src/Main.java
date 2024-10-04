17535b911c80579553e9264b6d6262f2775391fb
ramse <ramsesgv@hotmail.com>
2024-10-03 17:23:15 -0700

    imprimir mas caracteres

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
        String nombre = "Ramses";
         System.out.println(nombre.charAt(0));
+        System.out.println(nombre.charAt(1));
+        System.out.println(nombre.charAt(2));
+        System.out.println(nombre.charAt(3));
     }
 }
